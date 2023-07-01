cdbfff3dcfac598fa0677ae7f48cb490798c45c2
Mals13 <malumalvka@gmail.com>
2023-07-01 11:13:59 +0530

    new commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import image from '../assets/bgg.jpg'
+
+const Home = () => {
+    return (
+        <>
+            <div className='img'>
+                <img src={image} alt="" />
+            </div>
+            <div className='home'> Spotify</div>
+            <div className='premium'>Premium</div>
+            <div className='help'>Help</div>
+            <div className='download'>Download</div>
+            <div className='signup'>Sign up</div>
+            <div className='login'>Log In</div>
+        </>
+    )
+}
+
+export default Home
